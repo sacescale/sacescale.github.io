b23b39bb8ab7ccf0052e62ac7a55fa05240a06bc
sacescale <43556071+sacescale@users.noreply.github.com>
2018-11-05 09:31:55 -0200

    Update control.js

--- js/control.js
+++ js/control.js
@@ -44,94 +44,115 @@ function calcProAtivo(e) {
 // Calculo Form Receptivo
 
 const calcReceptivo = () => {
-    const tv = {
-        "Facil SD": 0.5,
-        "Facil HD": 0.7,
-        "Light HD": 1.35,
-        "Mix HD": 3,
-        "Top HD": 5
-    }
+    const tv = [
+        [
+            ["Facil SD", 0.5], ["Facil HD", 0.7], ["Light HD", 1.35], ["Mix HD", 3], ["Top HD", 5]
+        ], [
+            ["Compacto SD", 0], ["Plus HD", 0], ["Corp HD", 0], ["Corp HD Max", 0]
+        ]
+    ]
+    const internet = [
+        [
+            ["5MB", 0.26], ["10MB", 2], ["30MB", 3], ["100MB", 4], ["240MB", 5]
+        ], [
+            ["10MB CNPJ", 2], ["30MB CNPJ", 3], ["10MB Ip Fixo", 3], ["40MB CNPJ", 3], ["60MB CNPJ", 3], ["70Mb CNPJ", 4], ["120MB CNPJ", 4], ["30MB Ip Fixo", 4], ["35MB Presença Web", 4], ["140MB CNPJ", 5], ["60MB Ip Fixo", 5], ["240MB CNPJ", 5],
+        ]
+    ]
 
-    const internet = {
-        "5MB": 0.26,
-        "10MB": 2, //Esse valor até o momente, refere-se as internet de 10MB, 20MB, 35MB
-        "30MB": 3, //Esse valor até o momente, refere-se as internet de 30MB, 60MB, 70MB
-        "100MB": 4, //Esse valor até o momente, refere-se as internet de 100MB e 120MB
-        "240MB": 5
-    }
+    const telefone = [
+        [
+            ["Ilimitado Net", 0.04], ["Ilimitado Local", 0.5], ["Ilimitado Brasil 21", 0.75], ["Ilimitado Brasil Total 21", 1], ["Ilimitado Mundo Total 21", 2]
+        ], [
+            ["Local (1Linha)", 0.35], ["Brasil21 (1Linha)", 0.47], ["Brasil Total21 (1Linha)", 2], ["Local (2Linhas)", 2], ["Mundo Total21 (1Linha)", 3], ["Brasil21 (2Linhas)", 3], ["Local (4Linhas)", 5], ["Brasil Total21 (2Linhas)", 5], ["Mundo Total21 (2Linhas)", 7], ["Brasil21 (4Linhas)", 8], ["Local (8Linhas)", 11], ["Brasil21 (8Linhas)", 17]
+        ],
+    ]
 
-    const telefone = {
-        "Ilimitado Net": 0.04,
-        "Ilimitado Local": 0.5,
-        "Ilimitado Brasil 21": 0.75,
-        "Ilimitado Brasil Total 21": 1,
-        "Ilimitado Mundo Total 21": 2
-    }
+    const multi = [
+        ["Controle R$39,99", 3], ["Controle R$54,99", 4.84], ["Controle R$70,00", 7.09], ["6GB", 7.18], ["8GB", 7.28], ["10GB", 8.15], ["15GB", 10.73], ["25GB", 15.44], ["30GB", 18.22], ["50GB", 20.98]
+    ]
 
-    const multi = {
-        "Controle R$39,99": 3,
-        "Controle R$54,99": 4.84,
-        "Controle R$70,00": 7.09,
-        "6GB": 7.18,
-        "8GB": 7.28,
-        "10GB": 8.15,
-        "15GB": 10.73,
-        "25GB": 15.44,
-        "30GB": 18.22,
-        "50GB": 20.98
-    }
 
     const newClient = 3
 
     const calculoPacotes = () => {
         /* Valores sobre o pacote de TV */
-        let facilSd = document.getElementById('facilSd').value * tv["Facil SD"]
-        let facilHd = document.getElementById('facilHd').value * tv["Facil HD"]
-        let lightHd = document.getElementById('lightHd').value * tv["Light HD"]
-        let mixHd = document.getElementById('mixHd').value * tv["Mix HD"]
-        let topHd = document.getElementById('topHd').value * tv["Top HD"]
+        let facilSd = document.getElementById('facilSd').value * tv[0][0][1]
+        let facilHd = document.getElementById('facilHd').value * tv[0][1][1]
+        let lightHd = document.getElementById('lightHd').value * tv[0][2][1]
+        let mixHd = document.getElementById('mixHd').value * tv[0][3][1]
+        let topHd = document.getElementById('topHd').value * tv[0][4][1]
 
         const valueTotalTv = facilSd + facilHd + lightHd + mixHd + topHd
 
         /* Valores sobre o pacote de Internet */
 
-        let virtua5Mb = document.getElementById('virtua5').value * internet["5MB"]
-        let virtua10Mb = document.getElementById('virtua10').value * internet["10MB"]
-        let virtua20Mb = document.getElementById('virtua20').value * internet["10MB"]
-        let virtua30Mb = document.getElementById('virtua30').value * internet["30MB"]
-        let virtua35Mb = document.getElementById('virtua35').value * internet["10MB"]
-        let virtua60Mb = document.getElementById('virtua60').value * internet["30MB"]
-        let virtua70Mb = document.getElementById('virtua70').value * internet["30MB"]
-        let virtua100Mb = document.getElementById('virtua100').value * internet["100MB"]
-        let virtua120Mb = document.getElementById('virtua120').value * internet["100MB"]
-        let virtua240Mb = document.getElementById('virtua240').value * internet["240MB"]
+        let virtua5Mb = document.getElementById('virtua5').value * internet[0][0][1]
+        let virtua10Mb = document.getElementById('virtua10').value * internet[0][1][1]
+        let virtua20Mb = document.getElementById('virtua20').value * internet[0][1][1]
+        let virtua35Mb = document.getElementById('virtua35').value * internet[0][1][1]
+        let virtua30Mb = document.getElementById('virtua30').value * internet[0][2][1]
+        let virtua60Mb = document.getElementById('virtua60').value * internet[0][2][1]
+        let virtua70Mb = document.getElementById('virtua70').value * internet[0][2][1]
+        let virtua100Mb = document.getElementById('virtua100').value * internet[0][3][1]
+        let virtua120Mb = document.getElementById('virtua120').value * internet[0][3][1]
+        let virtua240Mb = document.getElementById('virtua240').value * internet[0][4][1]
+        /* Valores Empresarial*/
+        let virtua10MbCnpj = document.getElementById('virtua10cnpj').value * internet[1][0][1]
+        let virtua30MbCnpj = document.getElementById('virtua30cnpj').value * internet[1][1][1]
+        let virtua10ipfixo = document.getElementById('virtua10ipfixo').value * internet[1][2][1]
+        let virtua40cnpj = document.getElementById('virtua40cnpj').value * internet[1][3][1]
+        let virtua60cnpj = document.getElementById('virtua60cnpj').value * internet[1][4][1]
+        let virtua70cnpj = document.getElementById('virtua70cnpj').value * internet[1][5][1]
+        let virtua120cnpj = document.getElementById('virtua120cnpj').value * internet[1][6][1]
+        let virtua30ipfixo = document.getElementById('virtua30ipfixo').value * internet[1][7][1]
+        let virtua30presenca = document.getElementById('virtua30presenca').value * internet[1][8][1]
+        let virtua140cnpj = document.getElementById('virtua140cnpj').value * internet[1][9][1]
+        let virtua60ipfixo = document.getElementById('virtua60ipfixo').value * internet[1][10][1]
+        let virtua240cnpj = document.getElementById('virtua240cnpj').value * internet[1][11][1]
 
-        const valueTotalVirtua = virtua5Mb + virtua10Mb + virtua20Mb + virtua30Mb + virtua35Mb + virtua60Mb + virtua70Mb + virtua100Mb + virtua120Mb + virtua240Mb
 
+        const valueTotalVirtuaResid = virtua5Mb + virtua10Mb + virtua20Mb + virtua30Mb + virtua35Mb + virtua60Mb + virtua70Mb + virtua100Mb + virtua120Mb + virtua240Mb
+
+        const valueTotalVirtuaEmpre = virtua10MbCnpj + virtua30MbCnpj + virtua10ipfixo + virtua40cnpj + virtua60cnpj + virtua70cnpj + virtua120cnpj + virtua30ipfixo + virtua30presenca + virtua140cnpj + virtua60ipfixo + virtua240cnpj
 
         /* Valores sobre o pacote de Telefone */
 
-        let net = document.getElementById('net').value * telefone["Ilimitado Net"]
-        let local = document.getElementById('local').value * telefone["Ilimitado Local"]
-        let brasil21 = document.getElementById('brasil21').value * telefone["Ilimitado Brasil 21"]
-        let brasilTt21 = document.getElementById('brasilTt21').value * telefone["Ilimitado Brasil Total 21"]
-        let mundoTt21 = document.getElementById('mundoTt21').value * telefone["Ilimitado Mundo Total 21"]
+        let net = document.getElementById('net').value * telefone[0][0][1]
+        let local = document.getElementById('local').value * telefone[0][1][1]
+        let brasil21 = document.getElementById('brasil21').value * telefone[0][2][1]
+        let brasilTt21 = document.getElementById('brasilTt21').value * telefone[0][3][1]
+        let mundoTt21 = document.getElementById('mundoTt21').value * telefone[0][4][1]
 
-        const valueTotalTelefone = net + local + brasil21 + brasilTt21 + mundoTt21
+        /*Planos Empresarial */
+
+        let localOneLine = document.getElementById('localOneLine').value * telefone[1][0][1]
+        let brasilOneLine = document.getElementById('brasilOneLine').value * telefone[1][1][1]
+        let brasilTotalOneline = document.getElementById('brasilTotalOneline').value * telefone[1][2][1]
+        let localTwoLine = document.getElementById('localTwoLine').value * telefone[1][3][1]
+        let mundoTotalOneLine = document.getElementById('mundoTotalOneLine').value * telefone[1][4][1]
+        let brasilTwoLine = document.getElementById('brasilTwoLine').value * telefone[1][5][1]
+        let localFourLine = document.getElementById('localFourLine').value * telefone[1][6][1]
+        let brasilTotalTwoline = document.getElementById('brasilTotalTwoline').value * telefone[1][7][1]
+        let mundoTotalTwoLine = document.getElementById('mundoTotalTwoLine').value * telefone[1][8][1]
+        let brasilFourLine = document.getElementById('brasilFourLine').value * telefone[1][9][1]
+        let localEightLine = document.getElementById('localEightLine').value * telefone[1][10][1]
+        let brasilEightLine = document.getElementById('brasilEightLine').value * telefone[1][11][1]
+
+        const valueTotalTelefoneResid = net + local + brasil21 + brasilTt21 + mundoTt21
+        const valueTotalTelefoneEmpres = localOneLine + brasilOneLine + brasilTotalOneline + localTwoLine + mundoTotalOneLine + brasilTwoLine + localFourLine + brasilTotalTwoline + mundoTotalTwoLine + brasilFourLine + localEightLine + brasilEightLine
 
         /* Valores sobre o pacote de Multi */
 
-        let multiCtrl1 = document.getElementById('multiCtrl1').value * multi["Controle R$39,99"]
-        let multiCtrl2 = document.getElementById('multiCtrl2').value * multi["Controle R$54,99"]
-        let multiCtrl3 = document.getElementById('multiCtrl3').value * multi["Controle R$70,00"]
-        let multi6 = document.getElementById('multi6').value * multi["6GB"]
-        let multi8 = document.getElementById('multi8').value * multi["8GB"]
-        let multi10 = document.getElementById('multi10').value * multi["10GB"]
-        let multi15 = document.getElementById('multi15').value * multi["15GB"]
-        let multi25 = document.getElementById('multi25').value * multi["25GB"]
-        let multi30 = document.getElementById('multi30').value * multi["30GB"]
-        let multi50 = document.getElementById('multi50').value * multi["50GB"]
-
+        let multiCtrl1 = document.getElementById('multiCtrl1').value * multi[0][1]
+        let multiCtrl2 = document.getElementById('multiCtrl2').value * multi[1][1]
+        let multiCtrl3 = document.getElementById('multiCtrl3').value * multi[2][1]
+        let multi6 = document.getElementById('multi6').value * multi[3][1]
+        let multi8 = document.getElementById('multi8').value * multi[4][1]
+        let multi10 = document.getElementById('multi10').value * multi[5][1]
+        let multi15 = document.getElementById('multi15').value * multi[6][1]
+        let multi25 = document.getElementById('multi25').value * multi[7][1]
+        let multi30 = document.getElementById('multi30').value * multi[8][1]
+        let multi50 = document.getElementById('multi50').value * multi[9][1]
         const valueTotalMulti = multiCtrl1 + multiCtrl2 + multiCtrl3 + multi6 + multi8 + multi10 + multi15 + multi25 + multi30 + multi50
 
         /* valores sobre os novos domicilios */
@@ -140,7 +161,7 @@ const calcReceptivo = () => {
 
 
         function somaTotal() {
-            return valueTotalTv + valueTotalVirtua + valueTotalTelefone + valueTotalMulti + novoClientes
+            return valueTotalTv + valueTotalVirtuaResid + valueTotalTelefoneResid + valueTotalMulti + valueTotalVirtuaEmpre + novoClientes + valueTotalTelefoneEmpres
         }
         document.getElementById('valorTotal').innerHTML = `Valor a Receber R$ ${somaTotal().toFixed(2).replace('.', ',')}`
 
@@ -148,6 +169,13 @@ const calcReceptivo = () => {
 
     calculoPacotes()
 
+
+}
+
+function clearAll() {
+    document.getElementsByClassName("inputValue").value = 0
+
+    document.getElementById('valorTotal').innerHTML = `Valor Total`
 }
 
 
